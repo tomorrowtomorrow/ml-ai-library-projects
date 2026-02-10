10117eec72ea2a84e92a97704e54a3ffc2f333c6
SahandKhodayi <kingicebear8585@gmail.com>
2026-02-10 02:50:23 -0800

    Update derivative.py

--- calculus/derivative.py
+++ calculus/derivative.py
@@ -1,7 +1,9 @@
 
-def derivative(f,x,h=0.00001):
+def derivative(f,x,h=0.00001):#using limits to evaluete the dev func
     
     return (f(x+h)-f(x))/h
 
 def f(x):
-    return x**2
+    return x**2#function
+
+    #need to make the function dynamic in future projects as poly then use tri calcules to make fully use of dev
