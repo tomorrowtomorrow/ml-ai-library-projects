c4f380e1ac0567cf4bd533c26386bb6f56d86ab2
fire0ngit <kingicebear8585@gmail.com>
2026-02-22 18:19:04 -0800

    Update Dot_product.py

--- Linear Algebra/Dot_product.py
+++ Linear Algebra/Dot_product.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 a=np.array([[1,2],
               [3,4]])
@@ -40,16 +41,66 @@ class vector:
      
     def __init__(self,vec):
         self.vec=np.array(vec)
-
-class vec_mul:
-    def __init__(self):
-        pass
-
-
+   
     def add(self,vec1,vec2):
-        r1,c1=vec1.vec.shape()
-        r2,c2=vec2.vec.shape()
+        r1=len(vec1)
+        r2=len(vec2)
+        if r1!=r2:
+            print({"invalid vectors "})
+            return 0
+        tmp_vec=[]
+        for i in range(r1):
+            tmp_vec.append(vec1[i]+vec2[i])
+        return tmp_vec
+
+    def mul(self,vec1,vec2):
+        r1=len(vec1)
+        r2=len(vec2)
+        if r1!=r2:
+            print({"invalid vectors "})
+            return 0
+        tmp_vec=[]
+        for i in range(r1):
+            tmp_vec.append(vec1[i]*vec2[i])
+        return tmp_vec
+    def linear_comb(self,vec1,scale1,vec2,scale2):
+            r1=len(vec1)
+            r2=len(vec2)
+            if r1!=r2:
+                print({"invalid vectors!!! "})
+                return 0
+            tmp_vec=[]
+            for i in range(r1-1):
+                tmp_vec.append((scale1*vec1[i])+(scale2*vec2[i]))
+            return tmp_vec
         
+
+    def vector_norm(self,vec1):
+        tmp=0
+        for i in range(len(vec1)):
+            tmp+=vec1[i]**2
+        return tmp**1/2
+    
+    def angle(self,vec1,vec2):
+        tmp=0
+        dot=0
+        real=0
         
+        dot=sum(vec1[i] * vec2[i] for i in range(len(vec1)))
+        norm1 = self.vector_norm(vec1)
+        norm2 = self.vector_norm(vec2)
+        return math.acos(dot / (norm1 * norm2))
+
+    
+    def check_ort(self,vec1,vec2):
+        if sum(self.mul(vec1,vec2))==0:
+            print("orthangualy vector")
+            return True
+        return False
+    
+        
+
+        
+                
 
         
