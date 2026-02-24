56c4be8656c78067aa3049ab7648497f0f672ac8
fire0ngit <kingicebear8585@gmail.com>
2026-02-23 21:21:03 -0800

    updated

--- Linear Algebra/Dot_product.py
+++ Linear Algebra/Dot_product.py
@@ -25,16 +25,17 @@ def dot(matrix1,matrix2,r,c):
         tmp+=rov_v[i]*col_v[i]
     return tmp    
 
-def dot_matrix(matrix1,matrix2,target):
+def dot_matrix(matrix1,matrix2):
     rows1,cols1=matrix1.shape
     rows2,cols2=matrix2.shape 
     if cols1!=rows2:
         print("invalid matrix to product!!")
         return 0 
+    tmp=[]
     for i in range(rows1):
         for j in range(cols2):
-            target[i][j]=dot(matrix1,matrix2,i+1,j+1)
-    return target
+            tmp[i][j]=dot(matrix1,matrix2,i+1,j+1)
+    return tmp
 
           
 class vector:
@@ -42,63 +43,76 @@ class vector:
     def __init__(self,vec):
         self.vec=np.array(vec)
    
-    def add(self,vec1,vec2):
-        r1=len(vec1)
+    def add(self,vec2):
+        r1=len(self.vec)
         r2=len(vec2)
         if r1!=r2:
             print({"invalid vectors "})
             return 0
         tmp_vec=[]
         for i in range(r1):
-            tmp_vec.append(vec1[i]+vec2[i])
+            tmp_vec.append(self.vec[i]+vec2[i])
         return tmp_vec
 
-    def mul(self,vec1,vec2):
-        r1=len(vec1)
+    def mul(self,vec2):
+        r1=len(self.vec)
         r2=len(vec2)
         if r1!=r2:
             print({"invalid vectors "})
             return 0
         tmp_vec=[]
         for i in range(r1):
-            tmp_vec.append(vec1[i]*vec2[i])
+            tmp_vec.append(self.vec[i]*vec2[i])
         return tmp_vec
-    def linear_comb(self,vec1,scale1,vec2,scale2):
-            r1=len(vec1)
+    def linear_comb(self,scale1,vec2,scale2):
+            r1=len(self.vec)
             r2=len(vec2)
             if r1!=r2:
                 print({"invalid vectors!!! "})
                 return 0
             tmp_vec=[]
-            for i in range(r1-1):
-                tmp_vec.append((scale1*vec1[i])+(scale2*vec2[i]))
+            for i in range(r1):
+                tmp_vec.append((scale1*self.vec[i])+(scale2*vec2[i]))
             return tmp_vec
         
 
-    def vector_norm(self,vec1):
+    def vector_norm(self):
         tmp=0
-        for i in range(len(vec1)):
-            tmp+=vec1[i]**2
-        return tmp**1/2
+        for i in range(len(self.vec)):
+            tmp+=self.vec[i]**2
+        return tmp**0.5
     
-    def angle(self,vec1,vec2):
+    def angle(self,vec2):
         tmp=0
         dot=0
         real=0
         
-        dot=sum(vec1[i] * vec2[i] for i in range(len(vec1)))
-        norm1 = self.vector_norm(vec1)
+        dot=sum(self.vec[i] * vec2[i] for i in range(len(vec1)))
+        norm1 = self.vector_norm(self.vec)
         norm2 = self.vector_norm(vec2)
         return math.acos(dot / (norm1 * norm2))
 
     
-    def check_ort(self,vec1,vec2):
-        if sum(self.mul(vec1,vec2))==0:
+    def check_ort(self,vec2):
+        if sum(self.mul(self.vec,vec2))==0:
             print("orthangualy vector")
             return True
         return False
     
-        
+    def scale(vec,scale):
+        new_v=[]
+        for i in range(len(vec)):
+            new_v.append(scale*vec[i])
+        return new_v    
+
+    
+    def projection(self,vec2):
+        """
+        this is projection vecotor to another vector2
+        """
+        proj=self.scale(vec2,(dot_matrix(self.vec,vec2)/dot_matrix(self.vec,self.vec)))
+        return proj
+    
 
         
                 
